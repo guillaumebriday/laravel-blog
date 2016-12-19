b1d22a2ef05cbd44957501bced3abaefafeb51d8
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-19 14:21:07 +0100

    Adding create method in posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,4 +17,14 @@ class PostsController extends Controller
       $posts = Post::orderBy('posted_at', 'desc')->paginate(20);
       return view('home')->withPosts($posts);
   }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create(Request $request)
+  {
+    return view('posts.create');
+  }
 }
