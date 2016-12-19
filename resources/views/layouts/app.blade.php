9051fdffff86d99727ad0597ab22531b876c8cde
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-19 16:12:29 +0100

    Adding success alert in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,7 +78,16 @@
             </div>
         </nav>
 
-        @yield('content')
+        <div class="container">
+            @if (Session::has('success'))
+                <div class="alert alert-success alert-dismissible" role="alert">
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  {{ Session::get('success') }}
+                </div>
+            @endif
+
+            @yield('content')
+        </div>
     </div>
 
     <!-- Scripts -->
