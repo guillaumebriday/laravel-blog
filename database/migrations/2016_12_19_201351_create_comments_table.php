7bc3b79cb2f3b28cea58500d596faccec5bd7e5f
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-19 21:17:04 +0100

    Adding migration for comments

---
+++ database/migrations/2016_12_19_201351_create_comments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function(Blueprint $table)
+        {
+            $table->increments('id');
+
+            $table->integer('author_id')->unsigned()->default(0);
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->integer('post_id')->unsigned();
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+
+            $table->text('content');
+            $table->datetime('posted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
