4ce2be97af332f3cf1296929705ac6972b561998
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-30 00:27:36 +0100

    Adding copy task for bootstrap fonts

--- gulpfile.js
+++ gulpfile.js
@@ -15,5 +15,6 @@ require('laravel-elixir-vue-2');
 
 elixir(mix => {
     mix.sass('app.scss')
-       .webpack('app.js');
+       .webpack('app.js')
+       .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/', 'public/fonts/bootstrap');
 });
