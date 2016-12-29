d7f701aa3204d7710c613e6990edc77c6a345f4a
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-30 00:14:38 +0100

    Adding form to delete comment

--- resources/views/comments/_show.blade.php
+++ resources/views/comments/_show.blade.php
@@ -1,7 +1,17 @@
 <div class="panel panel-default">
     <div class="panel-heading">
         <span>{{ user_name($comment->author) }}</span>
-        <time class="pull-right">{{ humanize_date($comment->posted_at) }}</time>
+        <span class="pull-right">
+          <time>{{ humanize_date($comment->posted_at) }}</time>
+
+          @can('delete', $comment)
+            {!! Form::model($comment, ['method' => 'DELETE', 'route' => ['comments.destroy', $comment->id], 'class' => 'form-inline delete-confirmation']) !!}
+              <button type="submit" class="btn btn-link">
+                <span class="text-danger glyphicon glyphicon-remove" aria-hidden="true"></span>
+              </button>
+            {!! Form::close() !!}
+          @endcan
+        </span>
     </div>
     <div class="panel-body">
         {{ $comment->content }}
