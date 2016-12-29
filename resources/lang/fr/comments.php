37a890beb33ef08c54d2a9a552436819aeeeb7e1
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-29 21:00:49 +0100

    Adding comments form view

--- resources/lang/fr/comments.php
+++ resources/lang/fr/comments.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
-  'count' => ':count commentaire|:count commentaires'
+  'count' => ':count commentaire|:count commentaires',
+  'comment' => 'Commenter',
+  'add_comment' => 'Ajouter un commentaire',
+  'created' => 'Commentaire créé avec succès',
+
+  'attributes' => [
+    'content' => 'Contenu'
+  ],
+
+  'placeholder' => [
+    'content' => 'Votre commentaire'
+  ]
 ];
--- resources/views/comments/_list.blade.php
+++ resources/views/comments/_list.blade.php
@@ -1,5 +1,7 @@
 <h2>{{ trans_choice('comments.count', $comments->total()) }}</h2>
 
+@include ('comments/_form')
+
 @each('comments/_show', $comments, 'comment')
 
 <div class="text-center">
