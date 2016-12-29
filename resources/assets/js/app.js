62f4add097844d328fee56cc5b98886fbfb589f6
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-30 00:27:22 +0100

    Cleaning assets folder from useless files

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,20 +1,8 @@
-
 /**
- * First we will load all of this project's JavaScript dependencies which
- * include Vue and Vue Resource. This gives a great starting point for
- * building robust, powerful web applications using Vue and Laravel.
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
  */
 
-require('./bootstrap');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the body of the page. From here, you may begin adding components to
- * the application, or feel free to tweak this setup for your needs.
- */
-
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+window.$ = window.jQuery = require('jquery');
+require('bootstrap-sass');
