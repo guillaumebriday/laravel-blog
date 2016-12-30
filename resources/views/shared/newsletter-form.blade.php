1f81666d0a19f3ea3ec2f932ff6dba7de185cf92
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-30 19:01:29 +0100

    Correcting translation path

--- resources/views/shared/newsletter-form.blade.php
+++ resources/views/shared/newsletter-form.blade.php
@@ -1,6 +1,6 @@
 {!! Form::open(['route' => 'newsletter-subscriptions.store', 'method' => 'post', 'class' => 'navbar-form navbar-right']) !!}
   <div class="form-group">
-    {!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => trans('newsletter.email')]) !!}
+    {!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => trans('newsletter.placeholder')]) !!}
   </div>
 
   {!! Form::submit(trans('newsletter.subscribre'), ['class' => 'btn btn-default']) !!}
