6744b10ba5946e19fcaed3bb6f64f29ebccb5b47
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-30 02:46:44 +0100

    Adding translations for users

--- resources/lang/fr/comments.php
+++ resources/lang/fr/comments.php
@@ -3,6 +3,7 @@
 return [
   'count' => ':count commentaire|:count commentaires',
   'comment' => 'Commenter',
+  'last_comments' => 'Les derniers commentaires',
   'add_comment' => 'Ajouter un commentaire',
   'created' => 'Commentaire créé avec succès',
   'deleted' => 'Commentaire supprimé avec succès',
---
+++ resources/lang/fr/users.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'profil' => 'Mon profil',
+  'nb_of_posts' => "Nombre d'articles",
+  'nb_of_comments' => "Nombre de commentaires",
+
+  'attributes' => [
+    'name' => 'Nom',
+    'email' => 'Email'
+  ]
+];
