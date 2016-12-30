197c95540890f59c88c00fa59fa692a10c7edf4d
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-30 02:44:41 +0100

    Creating UsersController

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UsersRequest;
+use App\User;
+
+class UsersController extends Controller
+{
+  /**
+   * Display the specified resource.
+   */
+  public function show(Request $request, User $user)
+  {
+    $posts = $user->posts()->orderBy('posted_at', 'desc')->limit(5)->get();
+    $comments = $user->comments()->orderBy('posted_at', 'desc')->limit(5)->get();
+
+    return view('users.show')->withUser($user)->withPosts($posts)->withComments($comments);
+  }
+}
