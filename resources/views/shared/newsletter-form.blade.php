80e2fde64a02c471fd896808f46f383bf485a888
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-30 17:02:51 +0100

    Adding footer template

---
+++ resources/views/shared/newsletter-form.blade.php
@@ -0,0 +1,7 @@
+{!! Form::open(['route' => 'newsletter-subscriptions.store', 'method' => 'post', 'class' => 'navbar-form navbar-right']) !!}
+  <div class="form-group">
+    {!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => trans('newsletter.email')]) !!}
+  </div>
+
+  {!! Form::submit(trans('newsletter.subscribre'), ['class' => 'btn btn-default']) !!}
+{!! Form::close() !!}
