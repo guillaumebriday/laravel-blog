ce0ab718dd828db6e81b3c1d0bea6f15598eb98a
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-30 19:01:40 +0100

    Adding translations for newsletter

--- resources/lang/fr/newsletter.php
+++ resources/lang/fr/newsletter.php
@@ -2,6 +2,14 @@
 
 return [
   'subscribre' => "S'inscrire",
-  'email' => 'Votre email',
-  'created' => "Email ajouté à la newsletter avec succès"
+  'placeholder' => 'Votre email',
+  'created' => "Email ajouté à la newsletter avec succès",
+
+  'email' => [
+    'subject' => 'Newsletter du mois',
+    'welcome' => 'Bienvenue sur la Newsletter du mois !',
+    'description' => 'Voici la liste des :count derniers articles publiés au cours du mois',
+    'thanks' => 'Merci de vous être abonné à notre newsletter',
+    'unsubscribe' => 'Se désabonner'
+  ]
 ];
