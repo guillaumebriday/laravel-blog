6a2e4af5a0d262821ae2f4c242f32d673743d061
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-31 13:13:30 +0100

    Adding seeds in DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
+use App\User;
+use App\Comment;
+use App\NewsletterSubscription;
+use Faker\Factory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Factory::create();
+
+        factory(User::class, 10)
+           ->create()
+           ->each(function ($user) use ($faker) {
+                factory(Post::class, $faker->numberBetween(2,20))
+                    ->create([
+                        'author_id' => $user->id
+                    ])
+                    ->each(function ($post) use ($faker) {
+                        factory(Comment::class, $faker->numberBetween(10,60))
+                            ->create([
+                                'post_id' => $post->id
+                            ]);
+                    });
+            });
+
+        factory(NewsletterSubscription::class, $faker->numberBetween(10, 50))->create();
     }
 }
