6cd4469fc94380fab259d2606eaf3a542ef91475
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-31 15:01:14 +0100

    Adding UnsubscribeEmailNewsletter Job

---
+++ app/Jobs/UnsubscribeEmailNewsletter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\NewsletterSubscription;
+
+class UnsubscribeEmailNewsletter implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $email;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $email = $this->email;
+
+        $newsletterSubscription = NewsletterSubscription::where('email', $email)->first();
+        if ($newsletterSubscription)
+        {
+            $newsletterSubscription->delete();
+        }
+    }
+}
