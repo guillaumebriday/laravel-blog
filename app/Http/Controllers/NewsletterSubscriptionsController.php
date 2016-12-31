d7190ac83fb08aabdc9228146caddd6b79725415
Guillaume Briday <guillaumebriday@gmail.com>
2016-12-31 12:11:43 +0100

    Adding unsubscribe method

--- app/Http/Controllers/NewsletterSubscriptionsController.php
+++ app/Http/Controllers/NewsletterSubscriptionsController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\NewsletterSubscriptionRequest;
+use Illuminate\Support\Facades\Session;
 use App\NewsletterSubscription;
+use App\Jobs\UnsubscribeEmailNewsletter;
+use Validator;
 
 
 class NewsletterSubscriptionsController extends Controller
@@ -21,5 +24,29 @@ class NewsletterSubscriptionsController extends Controller
     ]);
 
     return back()->withSuccess(trans('newsletter.created'));
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  Request $request
+   * @return \Illuminate\Http\Response
+   */
+  public function unsubscribe(Request $request)
+  {
+      $validator = Validator::make($request->all(), [
+          'email' => 'required|email|exists:newsletter_subscriptions,email'
+      ]);
+
+      if ($validator->fails())
+      {
+        return redirect()->route('home')->withErrors($validator);
+      }
+
+      $this->dispatch(new UnsubscribeEmailNewsletter($request->input('email')));
+
+      Session::flash('success', trans('newsletter.unsubscribed'));
+
+      return view('newsletters.unsubscribed')->withSuccess(trans('newsletter.unsubscribed'));
   }
 }
