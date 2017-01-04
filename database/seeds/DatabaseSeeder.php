34a5c4de81c44a1b61a1b2758bb6f4d4d0493d50
Guillaume Briday <guillaumebriday@gmail.com>
2017-01-04 23:27:42 +0100

    Moving actual DatabaseSeeder.php into dev/DevDatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,11 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Post;
-use App\User;
-use App\Comment;
-use App\NewsletterSubscription;
-use Faker\Factory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,23 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Factory::create();
-
-        factory(User::class, 10)
-            ->create()
-            ->each(function ($user) use ($faker) {
-                factory(Post::class, $faker->numberBetween(2, 20))
-                    ->create([
-                        'author_id' => $user->id
-                    ])
-                    ->each(function ($post) use ($faker) {
-                        factory(Comment::class, $faker->numberBetween(10, 60))
-                            ->create([
-                                'post_id' => $post->id
-                            ]);
-                    });
-            });
-
-        factory(NewsletterSubscription::class, $faker->numberBetween(10, 50))->create();
+        //
     }
 }
---
+++ database/seeds/dev/DevDatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use App\User;
+use App\Comment;
+use App\NewsletterSubscription;
+use Faker\Factory;
+
+class DevDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+
+        factory(User::class, 10)
+            ->create()
+            ->each(function ($user) use ($faker) {
+                factory(Post::class, $faker->numberBetween(2, 20))
+                    ->create([
+                        'author_id' => $user->id
+                    ])
+                    ->each(function ($post) use ($faker) {
+                        factory(Comment::class, $faker->numberBetween(10, 60))
+                            ->create([
+                                'post_id' => $post->id
+                            ]);
+                    });
+            });
+
+        factory(NewsletterSubscription::class, $faker->numberBetween(10, 50))->create();
+    }
+}
