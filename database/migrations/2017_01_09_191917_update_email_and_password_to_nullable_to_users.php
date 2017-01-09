8192d78c9f9ef1cd39a92d147700167a5c674d2f
Guillaume Briday <guillaumebriday@gmail.com>
2017-01-09 20:26:26 +0100

    Adding migration to make the email and password nullable when provider is used to connect

---
+++ database/migrations/2017_01_09_191917_update_email_and_password_to_nullable_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateEmailAndPasswordToNullableToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->nullable()->change();
+            $table->string('password')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->nullable(false)->change();
+            $table->string('password')->nullable(false)->change();
+        });
+    }
+}
