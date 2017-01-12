60d38b316dbd8ee4fb63ba5f8115fe5f2a1512a1
Guillaume Briday <guillaumebriday@gmail.com>
2017-01-12 19:56:25 +0100

    Moving posts index view to specific file

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">{{ trans('posts.last_posts') }}</div>
 
                 <div class="panel-body">
                     @include ('posts/_list')
