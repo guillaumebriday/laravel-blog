7465e53f53d42093c0ddc73ddb63295e80098f28
Guillaume Briday <guillaumebriday@gmail.com>
2017-01-12 20:30:10 +0100

    Removing row and col classes from views of users

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,30 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <strong>{{ user_name($user) }}</strong>
-            </div>
-            <div class="panel-body">
-              @include ('users/_profil')
-            </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <strong>{{ user_name($user) }}</strong>
         </div>
+        <div class="panel-body">
+            @include ('users/_profil')
+        </div>
+    </div>
 
-        <ul class="nav nav-tabs">
-          <li class="active"><a data-toggle="pill" href="#posts">{{ trans('posts.last_posts')}}</a></li>
-          <li><a data-toggle="pill" href="#comments">{{ trans('comments.last_comments')}}</a></li>
-        </ul>
+    <ul class="nav nav-tabs">
+        <li class="active"><a data-toggle="pill" href="#posts">{{ trans('posts.last_posts')}}</a></li>
+        <li><a data-toggle="pill" href="#comments">{{ trans('comments.last_comments')}}</a></li>
+    </ul>
 
-        <div class="tab-content">
-          <div id="posts" class="tab-pane active">
+    <div class="tab-content">
+        <div id="posts" class="tab-pane active">
             @each('posts/user/_show', $posts, 'post')
-          </div>
-          <div id="comments" class="tab-pane">
-            @each('comments/user/_show', $comments, 'comment')
-          </div>
         </div>
-      </div>
-  </div>
+        <div id="comments" class="tab-pane">
+            @each('comments/user/_show', $comments, 'comment')
+        </div>
+    </div>
 @endsection
