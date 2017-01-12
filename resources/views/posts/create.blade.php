1eb30ec94a5dfc558451beae23b5743cd869b83c
Guillaume Briday <guillaumebriday@gmail.com>
2017-01-12 20:09:21 +0100

    Removing row and col classes from views of posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-          @include ('posts/_form')
-      </div>
-  </div>
+    @include ('posts/_form')
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,17 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ trans('posts.last_posts') }}</div>
+    <div class="panel panel-default">
+        <div class="panel-heading">{{ trans('posts.last_posts') }}</div>
 
-                <div class="panel-body">
-                    @include ('posts/_list')
-                </div>
-            </div>
+        <div class="panel-body">
+            @include ('posts/_list')
         </div>
     </div>
-</div>
 @endsection
