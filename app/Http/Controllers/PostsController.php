209d342543b71732b65f1004c5511f1fb1bb41ec
Guillaume Briday <guillaumebriday@gmail.com>
2017-01-12 19:56:48 +0100

    Calling posts.index view in PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,7 +17,7 @@ class PostsController extends Controller
     public function index()
     {
         $posts = Post::orderBy('posted_at', 'desc')->paginate(20);
-        return view('home')->withPosts($posts);
+        return view('posts.index')->withPosts($posts);
     }
 
     /**
