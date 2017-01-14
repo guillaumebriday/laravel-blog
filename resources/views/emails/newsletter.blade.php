580e31d03a2f5e3cb6f52f79c2c659733b7a49c8
Guillaume Briday <guillaumebriday@gmail.com>
2017-01-14 23:39:23 +0100

    Changing indentation for newsletter views

--- resources/views/emails/newsletter.blade.php
+++ resources/views/emails/newsletter.blade.php
@@ -1,19 +1,19 @@
 <h1>{{ trans('newsletter.email.welcome') }}</h1>
 
 <p>
-  {{ trans('newsletter.email.description', ['count' => $posts->count()]) }} :
+    {{ trans('newsletter.email.description', ['count' => $posts->count()]) }} :
 </p>
 
 <ul>
-  @foreach($posts as $post)
-    <li>{{ link_to_route('posts.show', $post->title, $post) }}</li>
-  @endforeach
+    @foreach($posts as $post)
+        <li>{{ link_to_route('posts.show', $post->title, $post) }}</li>
+    @endforeach
 </ul>
 
 <p>
-  {{ trans('newsletter.email.thanks') }}
+    {{ trans('newsletter.email.thanks') }}
 </p>
 
 <p>
-  {{ link_to_route('newsletter-subscriptions.unsubscribe', trans('newsletter.email.unsubscribe'), ['email' => $email]) }}
+    {{ link_to_route('newsletter-subscriptions.unsubscribe', trans('newsletter.email.unsubscribe'), ['email' => $email]) }}
 </p>
