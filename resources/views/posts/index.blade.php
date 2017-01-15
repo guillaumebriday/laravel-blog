95fc43605b852490a468120c832b041ced3b99fe
Guillaume Briday <guillaumebriday@gmail.com>
2017-01-15 19:34:30 +0100

    Adding link to rss feed in posts index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
     <div class="panel panel-default">
-        <div class="panel-heading">{{ trans('posts.last_posts') }}</div>
+        <div class="panel-heading">
+            {{ trans('posts.last_posts') }}
+            <a href="{{ route('posts.feed') }}" class="pull-right" data-turbolinks="false">
+                <i class="fa fa-rss" aria-hidden="true"></i>
+            </a>
+        </div>
 
         <div class="panel-body">
             @include ('posts/_list')
