2216dcda9cebe5d00dfbc5559c35c482ac2fef99
Guillaume Briday <guillaumebriday@gmail.com>
2017-01-23 23:40:22 +0100

    Adding CheckRole middleware

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckRole
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $role
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        if (! $request->user()->hasRole($role)) {
+            return redirect()->route('home')->withErrors(trans('auth.not_authorized'));
+        }
+
+        return $next($request);
+    }
+}
