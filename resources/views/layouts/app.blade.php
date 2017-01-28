12c2b40f53aeaa720a2648a22bfab56ab3028a83
Guillaume Briday <guillaumebriday@gmail.com>
2017-01-28 13:50:22 +0100

    Moving alerts in shared view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,24 +25,7 @@
         @include('shared/navbar')
 
         <div class="container">
-            @if (Session::has('success'))
-                <div class="alert alert-success alert-dismissible" role="alert">
-                  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                  {{ Session::get('success') }}
-                </div>
-            @endif
-
-            @if (Session::has('errors'))
-                <div class="alert alert-danger alert-dismissible" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    {{ trans_choice('validation.errors', $errors->count()) }}
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+            @include('shared/alerts')
 
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
