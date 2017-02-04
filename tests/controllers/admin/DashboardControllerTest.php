7edd498930688fd031c480245aee673083edd07a
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-04 16:28:06 +0100

    Adding test for DashboardController

--- tests/controllers/admin/DashboardControllerTest.php
+++ tests/controllers/admin/DashboardControllerTest.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 use App\User;
+use App\Role;
 
 class DashboardControllerTest extends BrowserKitTest
 {
@@ -15,5 +16,19 @@ class DashboardControllerTest extends BrowserKitTest
         $response = $this->actingAs($user)->call('GET', route('admin.dashboard'));
 
         $this->assertRedirectedToRoute('home');
+    }
+
+    public function testDashboard()
+    {
+        $user = factory(User::class)->create();
+        $role_admin = factory(Role::class)->create(['name' => 'admin']);
+        $user->roles()->sync([$role_admin->id]);
+
+        $response = $this->actingAs($user)->call('GET', route('admin.dashboard'));
+
+        $this->assertResponseOk();
+        $this->assertViewHas('posts');
+        $this->assertViewHas('comments');
+        $this->assertViewHas('users');
     }
 }
