84dc9275d4548bacb51abffdd4577d5de4f11b5c
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-09 01:40:44 +0100

    Adding routes to delete a post in admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::group(['prefix' => 'auth'], function () {
 
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin'], 'namespace' => 'Admin', 'as' => 'admin.'], function () {
     Route::get('/dashboard', 'DashboardController@dashboard')->name('dashboard');
-    Route::resource('posts', 'PostsController', ['only' => ['index', 'edit', 'update']]);
+    Route::resource('posts', 'PostsController', ['only' => ['index', 'edit', 'update', 'destroy']]);
 });
 
 Route::group(['middleware' => 'auth'], function () {
