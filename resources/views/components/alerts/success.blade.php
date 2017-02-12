f0816ec94ee7548fdb39d4fe371cb7137d402ccb
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-12 11:48:03 +0100

    Removing old alerts components

--- resources/views/components/alerts/success.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-success alert-dismissible" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    {{ $slot }}
-</div>
