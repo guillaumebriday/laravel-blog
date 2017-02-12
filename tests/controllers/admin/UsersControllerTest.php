c26538294e5f17c0db8705abeb32efe159209701
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-12 23:10:42 +0100

    Adding tests to update method

--- tests/controllers/admin/UsersControllerTest.php
+++ tests/controllers/admin/UsersControllerTest.php
@@ -30,4 +30,43 @@ class AdminUsersControllerTest extends BrowserKitTest
         $this->assertViewHas('roles');
         $this->assertViewHas('user');
     }
+
+    public function testUpdate()
+    {
+        $admin = factory(User::class)->create();
+        $admin->roles()->attach(factory(Role::class)->states('admin')->create());
+        $user = factory(User::class)->create();
+
+        $params = [
+            'name' => 'luke',
+            'email' => 'luke@rebels.y4'
+        ];
+
+        $response = $this->actingAs($admin)->call('PATCH', route('admin.users.update', $user->id), $params);
+
+        $user = $user->fresh();
+        $this->seeInDatabase('users', $params);
+        $this->assertEquals($params['email'], $user->email);
+        $this->assertResponseStatus('302');
+    }
+
+    public function testUpdateRoles()
+    {
+        $admin = factory(User::class)->create();
+        $admin->roles()->attach(factory(Role::class)->states('admin')->create());
+        $user = factory(User::class)->create();
+
+        $role_editor = factory(Role::class)->states('editor')->create();
+
+        $params = [
+            'name' => 'luke',
+            'email' => 'luke@rebels.y4',
+            'roles' => ['editor' => $role_editor->id]
+        ];
+
+        $response = $this->actingAs($admin)->call('PATCH', route('admin.users.update', $user->id), $params);
+
+        $this->assertRedirectedToRoute('admin.users.edit', ['id' => $user->id]);
+        $this->assertTrue($user->roles->pluck('id')->contains($role_editor->id));
+    }
 }
