283c2c6ed854e55c1a24f8cca75572e121bcb259
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-12 16:05:18 +0100

    Adding edit users route for admin

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::group(['prefix' => 'auth'], function () {
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin'], 'namespace' => 'Admin', 'as' => 'admin.'], function () {
     Route::get('/dashboard', 'DashboardController@dashboard')->name('dashboard');
     Route::resource('posts', 'PostsController', ['only' => ['index', 'edit', 'update', 'destroy']]);
-    Route::resource('users', 'UsersController', ['only' => ['index']]);
+    Route::resource('users', 'UsersController', ['only' => ['index', 'edit']]);
 });
 
 Route::group(['middleware' => 'auth'], function () {
