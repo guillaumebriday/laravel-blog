8116957c23be452308354c6ca176cfcc3203db9a
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-12 11:47:45 +0100

    Using dismissible alert component in alerts partial

--- resources/views/shared/alerts.blade.php
+++ resources/views/shared/alerts.blade.php
@@ -1,11 +1,11 @@
 @if (Session::has('success'))
-    @component('components.alerts.success')
+    @component('components.alerts.dismissible', ['type' => 'success'])
       {{ Session::get('success') }}
     @endcomponent
 @endif
 
 @if (Session::has('errors'))
-    @component('components.alerts.errors')
+    @component('components.alerts.dismissible', ['type' => 'danger'])
         {{ trans_choice('validation.errors', $errors->count()) }}
         <ul>
             @foreach($errors->all() as $error)
