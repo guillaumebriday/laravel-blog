46c663882bf4c9f66e900ebb9642ef9a9fa54b38
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-12 21:48:42 +0100

    Adding Admin\UsersRequest

---
+++ app/Http/Requests/Admin/UsersRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UsersRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . $this->user->id,
+            'password' => 'nullable|confirmed',
+            'roles.*' => 'exists:roles,id'
+        ];
+    }
+}
