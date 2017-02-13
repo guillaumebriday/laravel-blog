263c1414fb20e0cf4d75de4fdeed5159145b2fd9
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-13 21:54:19 +0100

    Adding test on edit users admin view

--- tests/views/admin/UsersViewsTest.php
+++ tests/views/admin/UsersViewsTest.php
@@ -20,4 +20,19 @@ class AdminUsersViewsTest extends BrowserKitTest
             ->click(user_name($user))
             ->seeRouteIs('admin.users.edit', $user);
     }
+
+    public function testUserEdit()
+    {
+        $faker = Factory::create();
+        $admin = factory(User::class)->create();
+        $admin->roles()->attach(factory(Role::class)->states('admin')->create());
+        $user = factory(User::class)->create();
+
+        $this->actingAs($admin)
+            ->visit(route('admin.users.edit', $user))
+            ->see(user_name($user))
+            ->see(__('forms.actions.update'))
+            ->see(__('roles.admin'))
+            ->see($user->email);
+    }
 }
