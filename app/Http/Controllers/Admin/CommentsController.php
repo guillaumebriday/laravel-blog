226492c2d09d90f9c221d985e22d1f15a095b912
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-14 00:00:24 +0100

    Adding the edit method in the Admin\CommentsController

--- app/Http/Controllers/Admin/CommentsController.php
+++ app/Http/Controllers/Admin/CommentsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Comment;
+use App\User;
 
 class CommentsController extends Controller
 {
@@ -17,5 +18,14 @@ class CommentsController extends Controller
         $comments = Comment::orderBy('posted_at', 'desc')->paginate(50);
 
         return view('admin.comments.index')->withComments($comments);
+    }
+
+    /**
+    * Display the specified resource edit form.
+    */
+    public function edit(Comment $comment)
+    {
+        $users = User::pluck('name', 'id');
+        return view('admin.comments.edit')->withComment($comment)->withUsers($users);
     }
 }
