fb4d97c400ec3ecdb18bd65c34d16058d5fa0ae3
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-14 00:35:16 +0100

    Correcting CS in create roles migration file

--- database/migrations/2017_01_23_210625_create_roles_table.php
+++ database/migrations/2017_01_23_210625_create_roles_table.php
@@ -13,7 +13,6 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
 
