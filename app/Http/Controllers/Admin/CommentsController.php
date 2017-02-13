4515c1293351e112d51460974250704480cf16a5
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-14 00:19:42 +0100

    Adding update method to Admin\CommentsController

--- app/Http/Controllers/Admin/CommentsController.php
+++ app/Http/Controllers/Admin/CommentsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\CommentsRequest;
 use App\Comment;
 use App\User;
+use Carbon\Carbon;
 
 class CommentsController extends Controller
 {
@@ -27,5 +29,16 @@ class CommentsController extends Controller
     {
         $users = User::pluck('name', 'id');
         return view('admin.comments.edit')->withComment($comment)->withUsers($users);
+    }
+
+    /**
+    * Update the specified resource in storage.
+    */
+    public function update(CommentsRequest $request, Comment $comment)
+    {
+        $request['posted_at'] = Carbon::createFromFormat('d/m/Y H:i:s', $request->input('posted_at'));
+        $comment->update($request->only(['content', 'posted_at', 'author_id']));
+
+        return redirect()->route('admin.comments.edit', $comment)->withSuccess(__('comments.updated'));
     }
 }
