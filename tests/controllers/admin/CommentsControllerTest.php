3891dc602a3b7d0dee47dd9d3407e7485017cad8
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-22 22:49:04 +0100

    Adding tests for updating comments in Admin\CommentsController

--- tests/controllers/admin/CommentsControllerTest.php
+++ tests/controllers/admin/CommentsControllerTest.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\User;
 use App\Role;
 use App\Comment;
+use Carbon\Carbon;
 
 class AdminCommentsControllerTest extends BrowserKitTest
 {
@@ -31,5 +32,56 @@ class AdminCommentsControllerTest extends BrowserKitTest
         $this->assertResponseOk();
         $this->assertViewHas('comment');
         $this->assertViewHas('users');
+    }
+
+    public function testUpdate()
+    {
+        $admin = factory(User::class)->create();
+        $admin->roles()->attach(factory(Role::class)->states('admin')->create());
+        $author = factory(User::class)->create();
+        $comment = factory(Comment::class)->create();
+
+        $format = 'd/m/Y H:i:s';
+
+        $params = [
+            'content' => "I'm a content",
+            'posted_at' => Carbon::tomorrow()->format($format),
+            'author_id' => $author->id,
+        ];
+
+        $response = $this->actingAs($admin)->call('PUT', route('admin.comments.update', $comment), $params);
+
+        $comment = $comment->fresh();
+        $params['posted_at'] = Carbon::createFromFormat($format, $params['posted_at']);
+
+        $this->seeInDatabase('comments', $params);
+        $this->assertEquals($params['content'], $comment->content);
+        $this->assertResponseStatus('302');
+    }
+
+    public function testUpdateFail()
+    {
+        $admin = factory(User::class)->create();
+        $admin->roles()->attach(factory(Role::class)->states('admin')->create());
+        $author = factory(User::class)->create();
+        $comment = factory(Comment::class)->create();
+
+        $format = 'd/m/Y H:i:s';
+
+        $params = [
+            'content' => "I'm a new content",
+            'posted_at' => Carbon::yesterday()->format($format),
+            'author_id' => $author->id,
+        ];
+
+        $response = $this->actingAs($admin)->call('PUT', route('admin.comments.update', $comment), $params);
+
+        $comment = $comment->fresh();
+
+        $params['posted_at'] = Carbon::createFromFormat($format, $params['posted_at']);
+
+        $this->notSeeInDatabase('comments', $params);
+        $this->assertSessionHasErrors('posted_at');
+        $this->assertResponseStatus('302');
     }
 }
