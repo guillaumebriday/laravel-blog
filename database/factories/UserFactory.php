52f3a5f6b5063f755de469caab47a8e67e4596d2
Guillaume Briday <guillaumebriday@gmail.com>
2017-02-25 20:30:26 +0100

    Adding anakin state to the user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,8 @@
 <?php
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+use App\User;
+
+$factory->define(User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
@@ -8,5 +10,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->state(User::class, 'anakin', function (Faker\Generator $faker) {
+    return [
+        'name' => 'Anakin',
+        'email' => 'anakin@skywalker.st'
     ];
 });
