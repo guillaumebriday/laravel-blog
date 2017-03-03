fca2a89a911bcc58c6688f8e612d57e1b4fc9141
Guillaume Briday <guillaumebriday@gmail.com>
2017-03-04 00:08:51 +0100

    Adding admin and user methods to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,31 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\User;
+use App\Role;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Return an admin user
+     * @return User $admin
+     */
+    protected function admin()
+    {
+        $admin = factory(User::class)->create();
+        $admin->roles()->attach(factory(Role::class)->states('admin')->create());
+
+        return $admin;
+    }
+
+    /**
+     * Return an user
+     * @return User
+     */
+    protected function user()
+    {
+        return factory(User::class)->create();
+    }
 }
