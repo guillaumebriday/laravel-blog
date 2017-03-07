4684e122b5dc5d105fea875588a4122f70e89c9b
Guillaume Briday <guillaumebriday@gmail.com>
2017-03-07 21:52:11 +0100

    Adding PHPDoc in Comment model

--- app/Comment.php
+++ app/Comment.php
@@ -28,17 +28,33 @@ class Comment extends Model
         'posted_at'
     ];
 
+    /**
+     * Scope a query to only include comments posted last week.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function scopeLastWeek($query)
     {
         return $query->whereBetween('posted_at', [Carbon::now()->subWeek(), Carbon::now()])
                      ->orderBy('posted_at', 'desc');
     }
 
+    /**
+    * Return the comment's author
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
     public function author()
     {
         return $this->belongsTo('App\User', 'author_id');
     }
 
+    /**
+    * Return the comment's post
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
     public function post()
     {
         return $this->belongsTo('App\Post');
