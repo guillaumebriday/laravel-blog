81d9e2e8301685775e1a3c9d7ca39f6f3690877f
Guillaume Briday <guillaumebriday@gmail.com>
2017-03-19 11:48:34 +0100

    Adding the method hasMedia in mediable trait

--- app/Concern/Mediable.php
+++ app/Concern/Mediable.php
@@ -6,6 +6,17 @@ trait Mediable
 {
 
     /**
+     * Check if the resource has a media
+     *
+     * @param integer $media_id
+     * @return boolean
+     */
+    public function hasMedia($media_id)
+    {
+        return $this->media()->where('id', $media_id)->exists();
+    }
+
+    /**
      * Get all of the resource's media.
      */
     public function media()
