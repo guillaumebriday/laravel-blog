751195d1985dcf1a879e8d79f6253aedd51c53c8
Guillaume Briday <guillaumebriday@gmail.com>
2017-03-26 23:54:27 +0200

    Adding response error methods

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -39,4 +39,51 @@ class ApiController extends Controller
     {
         return response()->json($data, $this->getStatusCode(), $headers);
     }
+
+    /**
+     * Return a 401 unauthorized error.
+     *
+     * @param string $message
+     * @return \Illuminate\Http\Response
+     */
+    public function respondUnauthorized($message = 'Unauthorized.')
+    {
+        return $this->setStatusCode(IlluminateResponse::HTTP_UNAUTHORIZED)->respondWithError($message);
+    }
+
+    /**
+     * Return a 403 forbidden error.
+     *
+     * @param string $message
+     * @return \Illuminate\Http\Response
+     */
+    public function respondForbidden($message = 'Forbidden.')
+    {
+        return $this->setStatusCode(IlluminateResponse::HTTP_FORBIDDEN)->respondWithError($message);
+    }
+
+    /**
+     * Return a 404 not found error
+     * @param  string $message
+     * @return \Illuminate\Http\Response
+     */
+    public function respondNotFound($message = 'Not found.')
+    {
+        return $this->setStatusCode(IlluminateResponse::HTTP_NOT_FOUND)->respondWithError($message);
+    }
+
+    /**
+     * Return an error response
+     * @param String $message
+     * @return \Illuminate\Http\Response
+     */
+    public function respondWithError($message)
+    {
+        return $this->respond([
+            'error' => [
+                'message' => $message,
+                'status' => $this->getStatusCode()
+            ]
+        ]);
+    }
 }
