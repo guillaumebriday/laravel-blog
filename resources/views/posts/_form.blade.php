23389794a0fd1cd6ad2ef5c083bdb7edc8d8bb06
Guillaume Briday <guillaumebriday@gmail.com>
2017-04-01 20:16:35 +0200

    Moving form creation outside of the form partial

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,23 +1,15 @@
-@component('components.panels.default')
-    @slot('title')
-        {{ __('posts.add_article') }}
-    @endslot
+<div class="form-group">
+  {!! Form::label('title', __('posts.attributes.title')) !!}
+  {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => __('posts.placeholder.title')]) !!}
+</div>
 
-    {!! Form::open(['route' => 'posts.store', 'method' => 'post', 'files' => true]) !!}
-        <div class="form-group">
-          {!! Form::label('title', __('posts.attributes.title')) !!}
-          {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => __('posts.placeholder.title')]) !!}
-        </div>
+<div class="form-group">
+    {!! Form::label('thumbnail', __('posts.attributes.thumbnail')) !!}
+    {!! Form::file('thumbnail', ['accept' => 'image/*']) !!}
+</div>
 
-        <div class="form-group">
-            {!! Form::label('thumbnail', __('posts.attributes.thumbnail')) !!}
-            {!! Form::file('thumbnail', ['accept' => 'image/*']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('content', __('posts.attributes.content')) !!}
-            {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => __('posts.placeholder.content')]) !!}
-        </div>
-        {!! Form::submit(__('posts.publish'), ['class' => 'btn btn-primary pull-right']) !!}
-    {!! Form::close() !!}
-@endcomponent
+<div class="form-group">
+    {!! Form::label('content', __('posts.attributes.content')) !!}
+    {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => __('posts.placeholder.content')]) !!}
+</div>
+{!! Form::submit(__('posts.publish'), ['class' => 'btn btn-primary pull-right']) !!}
