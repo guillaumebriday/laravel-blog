937eca23c16d1a12aa687f33d93943b3166e8661
Guillaume Briday <guillaumebriday@gmail.com>
2017-04-01 15:36:27 +0200

    Adding the Comment transformer

---
+++ app/Transformers/CommentTransformer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use App\Comment;
+
+class CommentTransformer extends TransformerAbstract
+{
+    /**
+     * Transform a comment.
+     *
+     * @param Comment $comment
+     * @return array
+     */
+    public function transform(Comment $comment)
+    {
+        return [
+            'id' => $comment->id,
+            'content' => $comment->content,
+            'posted_at' => $comment->posted_at->toIso8601String(),
+            'author_id' => $comment->author_id,
+            'post_id' => $comment->post_id
+        ];
+    }
+}
