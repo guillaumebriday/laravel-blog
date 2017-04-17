18cabc8f762ba6514b02bb517615968620331d77
Guillaume Briday <guillaumebriday@gmail.com>
2017-04-17 16:24:26 +0200

    Deleting useless post's methods

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
-use App\Http\Requests\PostsRequest;
 use App\Post;
 
 class PostsController extends Controller
@@ -42,80 +41,5 @@ class PostsController extends Controller
     {
         $comments = $post->comments()->latest()->paginate(50);
         return view('posts.show')->withPost($post)->withComments($comments);
-    }
-
-    /**
-    * Show the form for creating a new resource.
-    *
-    * @return Response
-    */
-    public function create(Request $request)
-    {
-        return view('posts.create');
-    }
-
-    /**
-    * Display the specified resource edit form.
-    *
-    * @return \Illuminate\Http\Response
-    */
-    public function edit(Post $post)
-    {
-        $this->authorize('update', $post);
-
-        return view('posts.edit')->withPost($post);
-    }
-
-    /**
-    * Store a newly created resource in storage.
-    *
-    * @return Response
-    */
-    public function store(PostsRequest $request)
-    {
-        $user = Auth::user();
-
-        $post = $user->posts()->create([
-            'title' => $request->input('title'),
-            'content' => $request->input('content')
-        ]);
-
-        if ($request->hasFile('thumbnail')) {
-            $post->storeAndSetThumbnail($request->file('thumbnail'));
-        }
-
-        return redirect()->route('posts.show', $post)->with('success', __('posts.created'));
-    }
-
-    /**
-    * Update the specified resource in storage.
-    *
-    * @return \Illuminate\Http\Response
-    */
-    public function update(PostsRequest $request, Post $post)
-    {
-        $this->authorize('update', $post);
-
-        $post->update($request->only(['title', 'content']));
-
-        if ($request->hasFile('thumbnail')) {
-            $post->storeAndSetThumbnail($request->file('thumbnail'));
-        }
-
-        return redirect()->route('posts.show', $post)->withSuccess(__('posts.updated'));
-    }
-
-    /**
-    * Unset the post's thumbnail.
-    *
-    * @return \Illuminate\Http\Response
-    */
-    public function destroy_thumbnail(Post $post)
-    {
-        $this->authorize('update', $post);
-
-        $post->update(['thumbnail_id' => null]);
-
-        return redirect()->route('posts.edit', $post)->withSuccess(__('posts.updated'));
     }
 }
