a0c6d27151a5b83229d5ac82efa6b0f3f59584ac
Guillaume Briday <guillaumebriday@gmail.com>
2017-09-01 17:35:59 +0200

    Updating auth views

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,40 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="panel panel-default">
-        <div class="panel-heading">{{ __('auth.reset_password') }}</div>
-        <div class="panel-body">
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
-                </div>
-            @endif
+<div class="row justify-content-md-center mt-5">
+    <div class="col-md-6">
+        <h1>{{ __('auth.reset_password') }}</h1>
 
-            <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                {{ csrf_field() }}
+        @if (session('status'))
+            @component('components.alerts.dismissible', ['type' => 'success'])
+                {{ session('status') }}
+            @endcomponent
+        @endif
 
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="col-md-4 control-label">{{ __('validation.attributes.email') }}</label>
+        {!! Form::open(['route' => 'password.email', 'role' => 'form', 'method' => 'POST']) !!}
+            <div class="form-group">
+                {!! Form::label('email', __('validation.attributes.email'), ['class' => 'control-label']) !!}
+                {!! Form::email('email', old('email'), ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''), 'required']) !!}
 
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback">{{ $errors->first('email') }}</span>
+                @endif
+            </div>
 
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                </div>
+            <div class="form-group">
+                {!! Form::submit(__('auth.send_password_reset_link'), ['class' => 'btn btn-primary']) !!}
+            </div>
 
-                <div class="form-group">
-                    <div class="col-md-6 col-md-offset-4">
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('auth.send_password_reset_link') }}
-                        </button>
-                    </div>
-                </div>
-            </form>
-        </div>
+        {!! Form::close() !!}
     </div>
+</div>
 @endsection
