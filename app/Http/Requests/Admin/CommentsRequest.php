54edbb054c00b859ed8360dbd15e985e49111694
Guillaume Briday <guillaumebriday@gmail.com>
2017-09-03 14:04:32 +0200

    Updating comments and posts request to work with default datetime format

--- app/Http/Requests/Admin/CommentsRequest.php
+++ app/Http/Requests/Admin/CommentsRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Carbon\Carbon;
 
 class CommentsRequest extends FormRequest
 {
@@ -17,17 +18,27 @@ class CommentsRequest extends FormRequest
     }
 
     /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'posted_at' => Carbon::parse($this->input('posted_at'))
+        ]);
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
     public function rules()
     {
-        $post = $this->comment->post;
-
         return [
             'content' => 'required|max:255',
-            'posted_at' => 'required|date_format:d/m/Y H:i:s|after_or_equal:' . $post->posted_at,
+            'posted_at' => 'required|after_or_equal:' . $this->comment->post->posted_at,
             'author_id' => 'required|exists:users,id'
         ];
     }
