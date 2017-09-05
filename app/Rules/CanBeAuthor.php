d0943580c057865aabda9b434013b21f06e4fa7f
Guillaume Briday <guillaumebriday@gmail.com>
2017-09-05 22:25:17 +0200

    Adding a Custom Validation Rule for CanBeAuthor

---
+++ app/Rules/CanBeAuthor.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use App\User;
+
+class CanBeAuthor implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $author = User::find($value);
+        return $author
+               ? $author->canBeAuthor()
+               : false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return trans('validation.can_be_author');
+    }
+}
