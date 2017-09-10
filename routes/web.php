4709f485861982ce39dd1f891d35895e7fb71ea2
Guillaume Briday <guillaumebriday@gmail.com>
2017-09-10 22:44:44 +0200

    Adding a route to destroy a post's thumbnail

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::prefix('auth')->group(function () {
 Route::prefix('admin')->middleware(['auth', 'role:admin'])->namespace('Admin')->as('admin.')->group(function () {
     Route::get('dashboard', 'ShowDashboard')->name('dashboard');
     Route::resource('posts', 'PostsController', ['only' => ['index', 'edit', 'update', 'destroy']]);
+    Route::delete('/posts/{post}/thumbnail', 'PostsThumbnailController@destroy')->name('posts_thumbnail.destroy');
     Route::resource('users', 'UsersController', ['only' => ['index', 'edit', 'update']]);
     Route::resource('comments', 'CommentsController', ['only' => ['index', 'edit', 'update', 'destroy']]);
 });
