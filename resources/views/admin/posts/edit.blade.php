8a095b39739c8a115a3d06d21730ee77a985896b
Guillaume Briday <guillaumebriday@gmail.com>
2017-09-10 22:43:58 +0200

    Adding thumbnail in edit post view

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,9 +1,17 @@
 @extends('admin.layouts.app')
 
 @section('content')
-    <div class="page-header">
-      <h1>{{ $post->title }} <small>{{ link_to_route('posts.show', __('posts.show'), $post) }}</small></h1>
-    </div>
+    <h1>{{ $post->title }} <small>{{ link_to_route('posts.show', __('posts.show'), $post) }}</small></h1>
+
+    @if ($post->hasThumbnail())
+        <p>
+            {{ Html::image($post->thumbnail()->url, $post->thumbnail()->original_filename, ['class' => 'img-thumbnail', 'width' => '350']) }}
+
+            {!! Form::model($post, ['method' => 'DELETE', 'route' => ['admin.posts_thumbnail.destroy', $post], 'data-confirm' => __('forms.posts.delete_thumbnail')]) !!}
+                {!! Form::button('<i class="fa fa-trash" aria-hidden="true"></i> ' . __('posts.delete_thumbnail'), ['class' => 'btn btn-link text-danger', 'name' => 'submit', 'type' => 'submit']) !!}
+            {!! Form::close() !!}
+        </p>
+    @endif
 
     @include('admin/posts/_form')
 @endsection
