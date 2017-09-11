eb20a2f3bcec04eb3b69b184d70b65ddb99a6f0b
Guillaume Briday <guillaumebriday@gmail.com>
2017-09-12 00:35:23 +0200

    Adding method to update a post via API

--- app/Http/Controllers/Api/V1/PostsController.php
+++ app/Http/Controllers/Api/V1/PostsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Transformers\PostTransformer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Admin\PostsRequest;
 use App\Post;
 use App\User;
 
@@ -31,6 +33,34 @@ class PostsController extends ApiController
         $posts = Post::withCount('comments')->latest()->paginate($request->input('limit', 20));
 
         return $this->paginatedCollection($posts);
+    }
+
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  App\Http\Requests\Admin\PostsRequest $request
+    * @param  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function update(PostsRequest $request, $id)
+    {
+        $post = Post::find($id);
+
+        if (! $post) {
+            return $this->respondNotFound();
+        }
+
+        if (! Auth::user()->can('update', $post)) {
+            return $this->respondUnauthorized();
+        }
+
+        $post->update($request->only(['title', 'content', 'posted_at', 'author_id']));
+
+        if ($request->hasFile('thumbnail')) {
+            $post->storeAndSetThumbnail($request->file('thumbnail'));
+        }
+
+        return $this->item($post);
     }
 
     /**
