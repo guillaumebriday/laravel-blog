9e32251286227c1b82d0afc1c99eb643ff479db6
Guillaume Briday <guillaumebriday@gmail.com>
2017-09-14 17:01:33 +0200

    Adding a policy to delete a post

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -42,4 +42,16 @@ class PostPolicy
     {
         return $user->isAdmin();
     }
+
+    /**
+     * Determine whether the user can delete the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function delete(User $user, Post $post)
+    {
+        return $user->isAdmin();
+    }
 }
