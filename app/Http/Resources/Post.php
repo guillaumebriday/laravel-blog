5fe598aaf9ffd0ae8c36f3ce5f4bc1235a98afca
Guillaume Briday <guillaumebriday@gmail.com>
2017-09-17 16:32:22 +0200

    Adding a Post Resource

---
+++ app/Http/Resources/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class Post extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+            'posted_at' => $this->posted_at->toIso8601String(),
+            'author_id' => $this->author_id,
+            'has_thumbnail' => $this->hasThumbnail(),
+            'thumbnail_url' => optional($this->thumbnail())->url,
+            'comments_count' => $this->comments_count ?? $this->comments()->count()
+        ];
+    }
+}
