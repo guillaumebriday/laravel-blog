f6273eb96d9ae9e4452011043aecd62bd2503fb0
Guillaume Briday <guillaumebriday@gmail.com>
2017-09-30 20:57:36 +0200

    Updating comments views to work with vuejs components

--- resources/views/comments/_list.blade.php
+++ resources/views/comments/_list.blade.php
@@ -1,9 +1,8 @@
-<h2 class="mt-2">{{ trans_choice('comments.count', $comments->total()) }}</h2>
+<h2 class="mt-2">{{ trans_choice('comments.count', $post->comments_count) }}</h2>
 
 @include ('comments/_form')
 
-@each('comments/_show', $comments, 'comment')
-
-<div class="d-flex justify-content-center mt-2">
-    {{ $comments->links() }}
-</div>
+<comment-list
+    post_id="{{ $post->id }}"
+    loading_comments="{{ __('comments.loading_comments') }}">
+</comment-list>
