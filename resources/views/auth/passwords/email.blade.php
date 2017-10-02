2dad575c5bde933bb35912a7a9f671e227aaaab1
Guillaume Briday <guillaumebriday@gmail.com>
2017-10-02 23:40:38 +0200

    Updating auth views style

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row justify-content-md-center mt-5">
+<div class="row justify-content-md-center m-3">
     <div class="col-md-6">
         <h1>{{ __('auth.reset_password') }}</h1>
 
