86025830adb20ebc27143204576e28bac2fe92d8
Guillaume Briday <guillaumebriday@gmail.com>
2017-10-03 19:14:29 +0200

    Adding trumbowyg as npm dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,6 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.sass('resources/assets/admin/sass/admin.scss', 'public/css');
+mix.js('resources/assets/admin/js/admin.js', 'public/js')
+   .sass('resources/assets/admin/sass/admin.scss', 'public/css')
+   .copy('node_modules/trumbowyg/dist/ui/icons.svg', 'public/js/ui/icons.svg');
