2fa585a89e04012304aa54491ebcea098d903b53
Guillaume Briday <guillaumebriday@gmail.com>
2017-10-03 19:33:59 +0200

    Using trumbowyg in admin post's form

--- resources/views/admin/posts/_form.blade.php
+++ resources/views/admin/posts/_form.blade.php
@@ -42,7 +42,7 @@
 
 <div class="form-group">
     {!! Form::label('content', __('posts.attributes.content')) !!}
-    {!! Form::textarea('content', null, ['class' => 'form-control' . ($errors->has('content') ? ' is-invalid' : ''), 'required' => 'required']) !!}
+    {!! Form::textarea('content', null, ['class' => 'form-control trumbowyg-form' . ($errors->has('content') ? ' is-invalid' : ''), 'required' => 'required']) !!}
 
     @if ($errors->has('content'))
         <span class="invalid-feedback">{{ $errors->first('content') }}</span>
