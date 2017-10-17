95c783989a24d5a76cd713bb7d47b908de653e3d
Guillaume Briday <guillaumebriday@gmail.com>
2017-10-17 13:29:47 +0200

    Adding the comment-form component

---
+++ resources/assets/js/components/Comment-form.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="form-group">
+            <textarea
+                class="form-control"
+                :class="{ 'is-invalid' : this.errors['content'] }"
+                :placeholder="placeholder"
+                v-model="content">
+            </textarea>
+
+            <span v-if="this.errors['content']" class="invalid-feedback">{{ this.errors['content'][0] }}</span>
+        </div>
+
+        <div class="form-group text-right">
+            <button class="btn btn-primary" @click="sendComment">
+                <i v-if="isLoading" class="fa fa-spinner fa-spin fa-fw"></i>
+                {{ button }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'post_id',
+            'placeholder',
+            'button'
+        ],
+
+        data() {
+            return {
+                content: '',
+                isLoading: false,
+                errors: []
+            }
+        },
+
+        methods: {
+            sendComment() {
+                this.isLoading = true
+
+                axios.post('/api/v1/posts/'+ this.post_id + '/comments', {
+                    content: this.content
+                })
+                .then(response => {
+                    this.$emit('added', response.data.data)
+
+                    this.content = ''
+                    this.isLoading = false
+                    this.errors = []
+                })
+                .catch(error => {
+                    this.isLoading = false
+                    this.errors = error.response.data.errors
+                })
+            }
+        }
+    }
+</script>
