da7eecf859586351d7a3a528779921e330de65d7
Guillaume Briday <guillaumebriday@gmail.com>
2017-10-17 14:08:13 +0200

    Updating comments views to work with vuejs

--- resources/views/comments/_form.blade.php
+++ resources/views/comments/_form.blade.php
@@ -1,23 +1,4 @@
-<h5>{{ __('comments.add_comment') }}</h5>
-
-@auth
-    {!! Form::open(['route' => 'comments.store', 'method' => 'post']) !!}
-        {!! Form::hidden('post_id', $post->id) !!}
-
-        <div class="form-group">
-            {!! Form::textarea('content', old('content'), ['class' => 'form-control' . ($errors->has('content') ? ' is-invalid' : ''), 'placeholder' => __('comments.placeholder.content'), 'rows' => '4', 'required']) !!}
-
-            @if ($errors->has('content'))
-                <span class="invalid-feedback">{{ $errors->first('content') }}</span>
-            @endif
-        </div>
-
-        <div class="form-group text-right">
-            {!! Form::submit(__('comments.comment'), ['class' => 'btn btn-primary']) !!}
-        </div>
-
-    {!! Form::close() !!}
-@else
+@guest
     @component('components.alerts.default', ['type' => 'warning'])
       {{ __('comments.sign_in_to_comment') }}
     @endcomponent
