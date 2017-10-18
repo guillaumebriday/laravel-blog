e940a82b41c0e35b675bc8e13527e14984af75c3
Guillaume Briday <guillaumebriday@gmail.com>
2017-10-18 09:55:53 +0200

    Authorizing post's route in channel

--- routes/channels.php
+++ routes/channels.php
@@ -14,3 +14,7 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('post.{postId}', function ($user, $postId) {
+    return true;
+});
