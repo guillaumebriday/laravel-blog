1ff4678418a6aa4a754eeb81536c60ecdbccf509
Guillaume Briday <guillaumebriday@gmail.com>
2017-11-26 17:59:27 +0100

    Refactoring user's routes for the current users only

--- routes/web.php
+++ routes/web.php
@@ -27,11 +27,17 @@ Route::prefix('admin')->middleware(['auth', 'role:admin'])->namespace('Admin')->
 });
 
 Route::middleware('auth')->group(function () {
-    Route::resource('users', 'UsersController', ['only' => ['edit', 'update']]);
-    Route::get('/users/{user}/password', 'UserPasswordsController@edit')->name('users.password');
-    Route::patch('/users/{user}/password', 'UserPasswordsController@update')->name('users.password.update');
-    Route::get('/users/{user}/token', 'UserTokensController@edit')->name('users.token');
-    Route::patch('/users/{user}/token', 'UserTokensController@update')->name('users.token.update');
+    Route::prefix('settings')->group(function () {
+        Route::get('account', 'UsersController@edit')->name('users.edit');
+        Route::match(['put', 'patch'], 'account', 'UsersController@update')->name('users.update');
+
+        Route::get('password', 'UserPasswordsController@edit')->name('users.password');
+        Route::match(['put', 'patch'], 'password', 'UserPasswordsController@update')->name('users.password.update');
+
+        Route::get('token', 'UserTokensController@edit')->name('users.token');
+        Route::match(['put', 'patch'], 'token', 'UserTokensController@update')->name('users.token.update');
+    });
+
     Route::resource('newsletter-subscriptions', 'NewsletterSubscriptionsController', ['only' => 'store']);
 });
 
