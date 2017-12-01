2b4c4212c055819b71902fb75f680d0ecebb81bd
Guillaume Briday <guillaumebriday@gmail.com>
2017-12-01 22:42:27 +0100

    Reducing DevDatabaseSeeder loading time

--- database/seeds/dev/DevDatabaseSeeder.php
+++ database/seeds/dev/DevDatabaseSeeder.php
@@ -3,8 +3,6 @@
 use App\Comment;
 use App\NewsletterSubscription;
 use App\Post;
-use App\User;
-use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class DevDatabaseSeeder extends Seeder
@@ -16,23 +14,15 @@ class DevDatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Factory::create();
-
-        factory(User::class, 10)
+        factory(Post::class, 20)
             ->create()
-            ->each(function ($user) use ($faker) {
-                factory(Post::class, $faker->numberBetween(2, 20))
+            ->each(function ($post) {
+                factory(Comment::class, 5)
                     ->create([
-                        'author_id' => $user->id
-                    ])
-                    ->each(function ($post) use ($faker) {
-                        factory(Comment::class, $faker->numberBetween(10, 60))
-                            ->create([
-                                'post_id' => $post->id
-                            ]);
-                    });
+                        'post_id' => $post->id
+                    ]);
             });
 
-        factory(NewsletterSubscription::class, $faker->numberBetween(10, 50))->create();
+        factory(NewsletterSubscription::class, 5)->create();
     }
 }
