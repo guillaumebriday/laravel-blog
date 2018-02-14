144e4e5fd83f83217849661f282907abdbcd67ed
x1m <martijn@x1m.nl>
2018-02-14 23:03:45 +0100

    Added regex to Auth/RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
+            'name' => 'required|string|max:255|regex:[A-Za-z1-9 ]',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
