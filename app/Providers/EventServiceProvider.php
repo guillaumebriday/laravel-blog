ba547c1529faa4a044769fe4fadf8edfe87cc6d1
Guillaume Briday <guillaumebriday@gmail.com>
2018-03-22 11:49:42 +0100

    Removing useless Listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\CommentPosted' => [
-            'App\Listeners\SendComment',
+        'App\Events\Event' => [
+            'App\Listeners\EventListener',
         ],
     ];
 
