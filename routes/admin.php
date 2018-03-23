e04b6f42d058905f15de1cdeda5fcbb19547c143
Guillaume Briday <guillaumebriday@gmail.com>
2018-03-23 09:20:50 +0100

    Adding routes for medialibrary

--- routes/admin.php
+++ routes/admin.php
@@ -5,3 +5,4 @@ Route::resource('posts', 'PostController');
 Route::delete('/posts/{post}/thumbnail', 'PostThumbnailController@destroy')->name('posts_thumbnail.destroy');
 Route::resource('users', 'UserController')->only(['index', 'edit', 'update']);
 Route::resource('comments', 'CommentController')->only(['index', 'edit', 'update', 'destroy']);
+Route::resource('media', 'MediaLibraryController')->only(['index', 'show', 'create', 'store', 'destroy']);
