fe340b9f71412e078389e5b688382be693fcc494
Guillaume Briday <guillaumebriday@gmail.com>
2018-03-27 00:12:57 +0200

    Using the socialite driver in stateless mode

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
     public function handleProviderCallback($provider)
     {
         try {
-            $user = Socialite::driver($provider)->user();
+            $user = Socialite::driver($provider)->stateless()->user();
         } catch (Exception $e) {
             return redirect()->route('login');
         }
