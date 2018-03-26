dabcf9926027a3f7cf299dce3344668e79582b24
Guillaume Briday <guillaumebriday@gmail.com>
2018-03-27 00:13:21 +0200

    Using the email if the name is not defined in the authController

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -54,7 +54,7 @@ class AuthController extends Controller
         }
 
         return User::create([
-                        'name' => $user->name,
+                        'name' => $user->name ?? $user->email,
                         'email' => $user->email,
                         'provider' => $provider,
                         'provider_id' => $user->id
