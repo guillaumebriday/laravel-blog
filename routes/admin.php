5a9a9921273a33b32d4012a86ca2be115c2dbcc4
Guillaume Briday <guillaumebriday@gmail.com>
2018-03-28 13:46:14 +0200

    Splitting routes in multiple files

---
+++ routes/admin.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::get('dashboard', 'ShowDashboard')->name('dashboard');
+Route::resource('posts', 'PostController');
+Route::delete('/posts/{post}/thumbnail', 'PostThumbnailController@destroy')->name('posts_thumbnail.destroy');
+Route::resource('users', 'UserController')->only(['index', 'edit', 'update']);
+Route::resource('comments', 'CommentController')->only(['index', 'edit', 'update', 'destroy']);
---
+++ routes/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+Auth::routes();
+
+Route::prefix('auth')->group(function () {
+    Route::get('{provider}', 'Auth\AuthController@redirectToProvider')->name('auth.provider');
+    Route::get('{provider}/callback', 'Auth\AuthController@handleProviderCallback');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::prefix('settings')->group(function () {
+        Route::get('account', 'UserController@edit')->name('users.edit');
+        Route::match(['put', 'patch'], 'account', 'UserController@update')->name('users.update');
+
+        Route::get('password', 'UserPasswordController@edit')->name('users.password');
+        Route::match(['put', 'patch'], 'password', 'UserPasswordController@update')->name('users.password.update');
+
+        Route::get('token', 'UserTokenController@edit')->name('users.token');
+        Route::match(['put', 'patch'], 'token', 'UserTokenController@update')->name('users.token.update');
+    });
+
+    Route::resource('newsletter-subscriptions', 'NewsletterSubscriptionController')->only('store');
+});
