9b2c7f0c080d25a56a33572eb1109feac358d17b
Guillaume Briday <guillaumebriday@gmail.com>
2018-04-20 22:48:38 +0200

    Removing Schema::defaultStringLength config for travis

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,10 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') == 'testing') {
-            // Because travis-ci is still not compatible with mysql 5.7 ...
-            Schema::defaultStringLength(191);
-        }
+        //
     }
 
     /**
