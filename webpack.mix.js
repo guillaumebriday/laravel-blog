c875501abb2662ace4a825511bb49d5ad148eeed
Guillaume Briday <guillaumebriday@gmail.com>
2018-04-30 10:29:33 +0200

    Versionning assets only in production

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,10 +13,12 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .version();
 
 mix.js('resources/assets/admin/js/admin.js', 'public/js')
    .sass('resources/assets/admin/sass/admin.scss', 'public/css')
-   .version();
 
-mix.copy('node_modules/trumbowyg/dist/ui/icons.svg', 'public/js/ui/icons.svg');
+mix.copy('node_modules/trumbowyg/dist/ui/icons.svg', 'public/js/ui/icons.svg')
+
+if (mix.inProduction()) {
+  mix.version()
+}
