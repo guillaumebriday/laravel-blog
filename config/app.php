5a971202c7daf1f837e1a0754390701083e6e581
Guillaume Briday <guillaumebriday@gmail.com>
2018-11-08 14:00:39 +0100

    Installing telescope

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\BladeServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
