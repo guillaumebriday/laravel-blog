c89fc6b55e2d116485725f8e1007b0f2a2481b40
Guillaume Briday <guillaumebriday@gmail.com>
2020-05-24 17:51:55 +0200

    Fixing return type in post thumbnail controller

--- app/Http/Controllers/Admin/PostThumbnailController.php
+++ app/Http/Controllers/Admin/PostThumbnailController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
-use Illuminate\Http\Response;
+use Illuminate\Http\RedirectResponse;
 
 class PostThumbnailController extends Controller
 {
     /**
      * Unset the post's thumbnail.
      */
-    public function destroy(Post $post): Response
+    public function destroy(Post $post): RedirectResponse
     {
         $post->update(['thumbnail_id' => null]);
 
