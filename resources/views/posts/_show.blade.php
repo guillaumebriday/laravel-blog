a0a7d3f7fd9bea1027e195a57e70f1550063aead
saber <saber.tabatabaee@gmail.com>
2021-12-25 23:38:07 +0330

    get thumb size for showing images in blog

--- resources/views/posts/_show.blade.php
+++ resources/views/posts/_show.blade.php
@@ -1,7 +1,7 @@
 <div class="card">
   @if ($post->hasThumbnail())
     <a href="{{ route('posts.show', $post)}}">
-      {{ Html::image($post->thumbnail->getUrl(), $post->name, ['class' => 'card-img-top']) }}
+      {{ Html::image($post->thumbnail->getUrl('thumb'), $post->name, ['class' => 'card-img-top']) }}
     </a>
   @endif
 
