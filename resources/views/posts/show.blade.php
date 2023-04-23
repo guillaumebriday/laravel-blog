31a22e67a9e52fcfc1d0bdbd112d45a8786a506f
saber <saber.tabatabaee@gmail.com>
2023-04-23 15:22:03 +0330

    add edit button for admin to edit all posts in post pages

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,32 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="bg-white p-3 post-card">
-    @if ($post->hasThumbnail())
-      {{ Html::image($post->thumbnail->getUrl('thumb'), $post->name, ['class' => 'card-img-top']) }}
-    @endif
+    @guest
+    @else
+    <a href="{{ route('admin.posts.edit', $post) }}" class="btn btn-primary btn-sm {{ request()->route()->named('admin.dashboard') ? 'active' : '' }}">
+        <i class="fa fa-pencil" aria-hidden="true"></i>
+    </a>
+    @endguest
+    <div class="bg-white p-3 post-card">
+        @if ($post->hasThumbnail())
+            {{ Html::image($post->thumbnail->getUrl('thumb'), $post->name, ['class' => 'card-img-top']) }}
+        @endif
 
-    <h1 v-pre>{{ $post->title }}</h1>
+        <h1 v-pre>{{ $post->title }}</h1>
 
-    <div class="mb-3">
-      <small v-pre class="text-muted">{{ link_to_route('users.show', $post->author->fullname, $post->author) }}</small>,
-      <small class="text-muted">{{ humanize_date($post->posted_at) }}</small>
+        <div class="mb-3">
+            <small v-pre
+                   class="text-muted">{{ link_to_route('users.show', $post->author->fullname, $post->author) }}</small>,
+            <small class="text-muted">{{ humanize_date($post->posted_at) }}</small>
+        </div>
+
+        <div v-pre class="post-content">
+            {!! $post->content !!}
+        </div>
+
+        <p class="mt-3">
+            <like
+                :likes-count="{{ $post->likes_count }}"
+                :liked="{{ json_encode($post->isLiked()) }}"
+                :item-id="{{ $post->id }}"
+                item-type="posts"
+                :logged-in="{{ json_encode(Auth::check()) }}"
+            />
+        </p>
     </div>
 
-    <div v-pre class="post-content">
-      {!! $post->content !!}
-    </div>
-
-    <p class="mt-3">
-      <like
-        :likes-count="{{ $post->likes_count }}"
-        :liked="{{ json_encode($post->isLiked()) }}"
-        :item-id="{{ $post->id }}"
-        item-type="posts"
-        :logged-in="{{ json_encode(Auth::check()) }}"
-      />
-    </p>
-  </div>
-
-  @include ('comments/_list')
+    @include ('comments/_list')
 @endsection
